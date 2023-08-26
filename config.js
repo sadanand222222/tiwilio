ae9a33d111f0b9b00b8fb7f494a4f54c70bb2fa1
sadanand222222 <138657290+sadanand222222@users.noreply.github.com>
2023-08-25 17:28:56 -0700

    API
    API  token

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  twilio: {
+    accountSid: 'AC02c672773f1ebe0652d671126c189f8e',
+    apiKey: 'SK684265157a93b99d3620cbd1cc6b0c68',
+    apiSecret:'zY0YunsT9r4PpuTWavfGaQgJx6FP6ERC'
+  }
+};
